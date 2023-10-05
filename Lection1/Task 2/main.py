26ee9df73052a2e466479420ea1c6c2e91877021
Okarpets <karpetsolekasndrschool10@gmail.com>
2023-10-05 17:48:56 +0300

    Added the second task of the first lection

---
+++ Lection1/Task 2/main.py
@@ -0,0 +1,8 @@
+
+print ("Please write a number 1")
+a = input ()
+a = int(a)
+print ("Please write a number 2")
+b = input ()
+b = int(b)
+print ("Oh, man, we with get a this, better number {} ! It is success.".format (b * a) )
