f79a00b10d1b09d947b089a19a4b636a856a49c5
Okarpets <karpetsolekasndrschool10@gmail.com>
2023-10-05 17:50:40 +0300

    The third task of the first lection

---
+++ Lection1/Task 3/main.py
@@ -0,0 +1,16 @@
+print ("Select number system:")
+print ("If you want to convert to Celsius, write - 1.")
+print ("If you want to convert to Fahrenheit, write - 2.")
+a = int(input())
+print ("Okay, write down the temperature.")
+b = int(input())
+print ("If the temperature written was in Celsius, write - 1, if Fahrenheit - 2.")
+c = int(input())
+if a == 1 and c == 1:
+    print (" {}".format (b))
+if a == 2 and c == 1:
+    print (" {}".format ((b * 9/5) + 32))
+if a == 1 and c == 2:
+    print (" {}".format ((b - 32) * 5/9))
+if a == 2 and c == 2:
+    print (" {}".format (b))
