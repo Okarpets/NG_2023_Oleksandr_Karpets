0e000805ad49ee235ca891f994f8d70647b90cec
Okarpets <karpetsoleksandrschool10@gmail.com>
2023-10-11 16:37:06 +0300

    Added fall

--- Lection1/Task 2/main.py
+++ Lection1/Task 2/main.py
@@ -1,2 +1,2 @@
 print ("Now we will pluses your numbers, write me two numbers, please.")
-print ("It's a your sum. = {}. Take it :3".format (int(input ()) + int(input ())))
+print ("It's a your sum.= {}.Take it :3".format(int(input())+int(input())))
