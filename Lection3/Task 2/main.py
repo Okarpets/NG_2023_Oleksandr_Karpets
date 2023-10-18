b0b22fa0faecbe5fa5b257d4f298a6d437900bca
Okarpets <karpetsoleksandrschool10@gmail.com>
2023-10-18 23:10:18 +0300

    23:10

---
+++ Lection3/Task 2/main.py
@@ -0,0 +1,15 @@
+import pip
+import json
+import psutil
+import socket
+from platform import uname
+print("Your system: " + uname().system, uname().release)
+print("Your PC name: " + uname().node)
+print("Proccesor name:" + uname().processor)
+print("Number of processor cores: " + str(psutil.cpu_count(logical=True)))
+print("CPU frequency (Rec, Min, Max): " + str(psutil.cpu_freq()))
+print("RAM size:" + str(psutil.virtual_memory().total))
+print("RAM size you can use now:" +  str(psutil.virtual_memory().available))
+print("RAM size what you use now:" + str(psutil.virtual_memory().used))
+print("Machine architecture:" + uname().machine)
+print("Your ip address: " + socket.gethostbyname(socket.gethostname()))
