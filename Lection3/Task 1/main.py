1c5c0205ae294e01528f0c935293b9408d928774
Okarpets <karpetsoleksandrschool10@gmail.com>
2023-10-21 00:14:53 +0300

    Added the first task of the thirty lection

--- Lection3/Task 1/main.py
+++ Lection3/Task 1/main.py
@@ -1,5 +1,10 @@
-import locale
-file1 = open(input(), 'r')
-line = file1.readline()
-print(line)
-file1.close()
+file1 = open(input('Write a file, what we have to open:\n'), "r")
+Ulst = file1.readline()
+print("Char's in your file:", + len(Ulst))
+i = 0
+lst = {}
+while i < len(Ulst):
+    lst[Ulst[i]] = (Ulst.count("{}".format(Ulst[i])))
+    i = i + 1
+    
+print("It's a value what you want to see:", + lst[input('Write a key what value you want to see:\n')])
