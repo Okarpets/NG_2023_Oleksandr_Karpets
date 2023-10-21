4876df264aebe0fac1728ef90f831294f8ad155f
Okarpets <karpetsoleksandrschool10@gmail.com>
2023-10-21 23:18:04 +0300

    Switched to re.sub

--- Lection2/Task 4/main.py
+++ Lection2/Task 4/main.py
@@ -1,5 +1,4 @@
-print("Write your own list:")
-vowels = input()
-print(vowels.translate({ord('b'): None, ord('B'): None, ord('c'): None, ord('C'): None, ord('d'): None, ord('D'): None, ord('f'): None,ord('F'): None, ord('g'): None, ord('G'): None, ord('h'): None,ord('H'): None, ord('j'): None, ord('J'): None, ord('k'): None, ord('K'): None, ord('l'): None, ord('L'): None, ord('m'): None,ord('M'): None, ord('n'): None, ord('N'): None, ord('p'): None,ord('P'): None, ord('q'): None, ord('Q'): None, ord('r'): None, ord('R'): None, ord('s'): None, ord('S'): None, ord('t'): None,ord('T'): None, ord('v'): None, ord('W'): None, ord('x'): None,ord('Z'): None}))
-#The code above simply removes all specified letters from the string except vowels, replacing them with: None 
-      
+import re
+lstuser = input('Write your lst:\n')
+without = re.sub("[B,b,C,c,D,d,F,f,G,g,H,h,J,j,K,k,L,l,M,m,N,n,P,p,Q,q,R,r,S,s,T,t,V,v,W,w,X,x,Y,y,Z,z]", "", lstuser)
+print("It's your list without consonants vowels: {}". format(without))
