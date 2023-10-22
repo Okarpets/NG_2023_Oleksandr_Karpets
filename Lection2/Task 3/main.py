93b6d7297b3ec8258ae1508889b5242e564cb41d
Okarpets <karpetsoleksandrschool10@gmail.com>
2023-10-22 12:44:07 +0300

    Added third task of the second lection

--- Lection2/Task 3/main.py
+++ Lection2/Task 3/main.py
@@ -1,5 +1,25 @@
-usernumbs = ()
-usernumbs = (input().split(" "))
-print(",\n".join(map(str,usernumbs)))
-if usernumbs[0] == str(10):
-        print("Tiem")
+n = int(input('Write your interval:\n'))
+print("Just list of number:")
+check = 0
+for i in range(n):
+  check = check + 1
+  if n % check == 0:
+    print(check, check)
+  else:
+     print(check, "-")
+
+
+print("Prime numbers in your interval:")
+a = [check for check in range(n + 1)]
+a[1] = 0
+check = 2
+while check <= n:
+    if a[check] != 0:
+        j = check + check
+        while j <= n :
+            a[j] = 0
+            j = j + check
+    check += 1
+a = set(a)
+a.remove(0)
+print(a)
